a2fd8b2535841fb2b0678430bad0c35fb2d5b44c
Aditya Chandra <inibukanadit@gmail.com>
2018-10-21 13:42:43 +0700

    added implementation

---
+++ app/src/main/java/me/inibukanadit/made/RandomNumberWidget.java
@@ -0,0 +1,41 @@
+package me.inibukanadit.made;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class RandomNumberWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        String lastUpdate = "Random : " + NumberGenerator.Generate(100);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.random_number_widget);
+
+        views.setTextViewText(R.id.appwidget_text, lastUpdate);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
