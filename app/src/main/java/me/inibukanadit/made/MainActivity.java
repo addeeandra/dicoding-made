078e4aae4936c59acbb9a5c20d19cc283e121f57
Aditya Chandra <inibukanadit@gmail.com>
2018-10-21 13:59:57 +0700

    add start and stop button on mainactivity

--- app/src/main/java/me/inibukanadit/made/MainActivity.java
+++ app/src/main/java/me/inibukanadit/made/MainActivity.java
@@ -1,13 +1,66 @@
 package me.inibukanadit.made;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnStart;
+    private Button mBtnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnStart = findViewById(R.id.btn_start);
+        mBtnStop = findViewById(R.id.btn_stop);
+
+        mBtnStart.setOnClickListener(this);
+        mBtnStop.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.btn_start:
+                startJob();
+                break;
+            case R.id.btn_stop:
+                stopJob();
+                break;
+        }
+    }
+
+    private static int JOB_ID = 100;
+    private static int PERIOD_SCHEDULER = 86000;
+
+    private void startJob() {
+        ComponentName serviceComponent = new ComponentName(this, UpdateWidgetService.class);
+        JobInfo jobInfo = new JobInfo.Builder(JOB_ID, serviceComponent)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE)
+                .setPeriodic(PERIOD_SCHEDULER)
+                .build();
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(jobInfo);
+
+        Toast.makeText(this, "Job started", Toast.LENGTH_SHORT).show();
+    }
+
+    private void stopJob() {
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.cancel(JOB_ID);
+
+        Toast.makeText(this, "Job stopped", Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/me/inibukanadit/made/UpdateWidgetService.java
+++ app/src/main/java/me/inibukanadit/made/UpdateWidgetService.java
@@ -2,12 +2,28 @@ package me.inibukanadit.made;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.widget.RemoteViews;
 
 public class UpdateWidgetService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+
+        RemoteViews views = new RemoteViews(getPackageName(), R.layout.random_number_widget);
+        ComponentName widget = new ComponentName(this, RandomNumberWidget.class);
+
+        String lastUpdate = "Random : " + NumberGenerator.Generate(100);
+
+        views.setTextViewText(R.id.appwidget_text, lastUpdate);
+
+        manager.updateAppWidget(widget, views);
+
+        jobFinished(params, false);
+
+        return true;
     }
 
     @Override
